f1e315a00b5c1e8756c7d893026aa1afe52fe431
dimput <dimasputra@mail.ugm.ac.id>
2020-09-09 23:11:27 +0700

    first commit

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+class LoginController extends BaseController
+{
+	public function index()
+	{
+		return view('pages/login.php');
+	}
+	//--------------------------------------------------------------------
+
+}
